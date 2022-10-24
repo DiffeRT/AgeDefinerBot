4cdd27562bd8910b4f35b78319c08557aa8974dd
Rustem Berkaliev <rustem.berkaliev@gmail.com>
2022-10-25 01:06:35 +0300

    unit tests

--- src/test/java/DateUtilsTest.java
+++ src/test/java/DateUtilsTest.java
@@ -14,8 +14,10 @@ public class DateUtilsTest {
         Assert.assertEquals(result, expected, message);
     }
 
-    @Test
-    public void testGetMonth() {
+    @Test(description = "Check getting number of the month", dataProvider = "getMonthInput")
+    public void testGetMonth(Date input, int expected, String message) {
+        int result = DateUtils.getMonth(input);
+        Assert.assertEquals(result, expected, message);
     }
 
     @Test
@@ -29,11 +31,20 @@ public class DateUtilsTest {
     @DataProvider
     public Object[][] getDayInput() {
         return new Object[][] {
-                {Date.from(new GregorianCalendar(120, Calendar.JANUARY, 1).toInstant()),  1, "Get 1st day"},  //y + 1900
-                {Date.from(new GregorianCalendar(121, Calendar.FEBRUARY, 28).toInstant()), 28, "Get last day of Feb 2021"},
-                {Date.from(new GregorianCalendar(120, Calendar.FEBRUARY, 29).toInstant()), 29, "Get last day of Feb 2020"},
-                {Date.from(new GregorianCalendar(122, Calendar.SEPTEMBER, 30).toInstant()), 30, "Get last day of Sep"},
-                {Date.from(new GregorianCalendar(122, Calendar.OCTOBER, 31).toInstant()), 31, "Get last day of Oct"}
+                {Date.from(new GregorianCalendar(120, Calendar.JANUARY, 1).toInstant()),  1, "Get 1st day"},                 //01.01.2020 (y == y + 1900)
+                {Date.from(new GregorianCalendar(121, Calendar.FEBRUARY, 28).toInstant()), 28, "Get last day of Feb 2021"},  //28.02.2021
+                {Date.from(new GregorianCalendar(120, Calendar.FEBRUARY, 29).toInstant()), 29, "Get last day of Feb 2020"},  //29.02.2020
+                {Date.from(new GregorianCalendar(122, Calendar.SEPTEMBER, 30).toInstant()), 30, "Get last day of Sep"},      //30.09.2022
+                {Date.from(new GregorianCalendar(122, Calendar.OCTOBER, 31).toInstant()), 31, "Get last day of Oct"}         //31.10.2022
+        };
+    }
+
+    @DataProvider
+    public Object[][] getMonthInput() {
+        return new Object[][] {
+                {Date.from(new GregorianCalendar(122, Calendar.JANUARY, 1).toInstant()),  1, "Get 1st month"},     //01.01.2022
+                {Date.from(new GregorianCalendar(122, Calendar.JUNE, 30).toInstant()),  6, "Get 6th month"},       //30.06.2022
+                {Date.from(new GregorianCalendar(122, Calendar.DECEMBER, 31).toInstant()),  12, "Get 12th month"}  //31.12.2022
         };
     }
 }
